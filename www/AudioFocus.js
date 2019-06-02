202d437e30efbcd5984eeb97bb833394013d36c7
JOOBA <33761785+joobasas@users.noreply.github.com>
2019-06-02 15:02:10 +0100

    Update AudioFocus.js

--- www/AudioFocus.js
+++ www/AudioFocus.js
@@ -3,6 +3,14 @@ var exec = require('cordova/exec');
 exports.requestFocus = function(success, error) {
     success = success || function() { };
     error = error || function() { };
-
+    
     exec(success, error, "AudioFocus", "requestFocus", []);
+   
+};
+
+exports.abandonFocus = function(success, error) {
+    success = success || function() { };
+    error = error || function() { };
+    
+    exec(success, error, "AudioFocus", "abandonFocus", []);
 };
